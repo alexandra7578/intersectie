1858d0a3a6cc80d7b2a0238850adfb5f6ed1c352
alexandra7578 <110526731+alexandra7578@users.noreply.github.com>
2023-11-01 14:13:43 +0200

    Add files via upload

--- tracker.py
+++ tracker.py
@@ -1,58 +1,91 @@
 import math
+class TrackObj:
+    def __init__(self):
+        self.box = None
+        self.first_occurrence_time = None
+        self.center_point = None
 
+    def update_box(self, new_box):
+        self.box = new_box
 
 class Tracker:
+
     def __init__(self):
-        # Store the center positions of the objects
-        self.center_points = {}
-        # Keep the count of the IDs
-        # each time a new object id detected, the count will increase by one
-        self.id_count = 0
+        self.count=0
+        self.center_points_prev_frame = []
+        self.tracking_objects ={} #lista de trackobj
+        self.track_id=0
+    def get_tracked_obj(self):
+        return self.tracking_objects
+
+    def update(self, boxes):
+        self.count+=1
+        center_points_cur_frame=[]
+
+        #collect all centraer points of the carent box detection
+        for box in boxes:
+
+            (x1, y1, x2, y2, id) = box
+
+            cx = int((x1 + x2 ) / 2)
+            cy = int((y1 + y2) / 2)
+
+            center_points_cur_frame.append((cx, cy))
+            # print("FRAME N: ", self.count, " ", x, y, w, h)
+        # Only at the beginning we compare previous and current frame
+
+        if self.count <= 2:
+            for pt in center_points_cur_frame:
+                for pt2 in self.center_points_prev_frame:
+                    distance = math.hypot(pt2[0] - pt[0], pt2[1] - pt[1])
+                    # for pt
+                    if distance < 50:
+                        self.tracking_objects[self.track_id] = pt
+                        self.track_id += 1
+            print("Tracking objects")
+            print(self.tracking_objects)
+        else:
+
+            tracking_objects_copy = self.tracking_objects.copy()
+            print(tracking_objects_copy)
+            center_points_cur_frame_copy = center_points_cur_frame.copy()
+            print(len(tracking_objects_copy))
+            for object_id, pt2 in tracking_objects_copy.items():
+                object_exists = False
+                pt_closest=(1000,1000)
+                distance_smalest=1000
+                for pt in center_points_cur_frame_copy:
+                    distance_tracked_2_curent = math.hypot(pt2[0] - pt[0], pt2[1] - pt[1])
+                    # un for pt cea mai mica distanta
+                    # Update IDs position
+                    if distance_tracked_2_curent<distance_smalest:
+                        distance_smalest=distance_tracked_2_curent
+                        pt_closest=pt
+                print(distance_smalest)
+                if distance_smalest < 50:
+                    self.tracking_objects[object_id] = pt_closest
+                    object_exists = True
+                    if pt_closest in center_points_cur_frame:
+                        center_points_cur_frame.remove(pt_closest)
+
+                # Remove IDs lost
+                if not object_exists:
+                    self.tracking_objects.pop(object_id)
+
+            # Add new IDs found
+            for pt in center_points_cur_frame:
+                self.tracking_objects[self.track_id] = pt
+                self.track_id += 1
 
 
-    def update(self, objects_rect):
 
-        # Get center point of new object
-        for rect in objects_rect:
-            x=rect[0]
-            y=rect[1]
-            w=rect[2]
-            h=rect[3]
-            cx = (x + x + w) // 2
-            cy = (y + y + h) // 2
+        # print("Tracking obj")
+        # print(self.tracking_objects)
+        #
+        # print("CUR FRAME")
+        # print(self.center_points_cur_frame)
+        #
+        # print("PREV FARME")
+        # print(self.center_points_prev_frame)
 
-            # Find out if that object was detected already
-            same_object_detected = False
-            smallestdist=50
-            for id, pt in self.center_points.items():
-                dist = math.hypot(cx - pt[0], cy - pt[1])
-                # print(dist)
-                if dist>=smallestdist:
-                    continue
-                if dist < smallestdist:
-                    smallestdist=dist
-                    self.center_points[id] = (cx, cy)
-#                    print(self.center_points)
-#                     objects_bbs_ids.append([x, y, w, h, id])
-                    rect[4]=id
-                    same_object_detected = True
-                    break
-
-            # New object is detected we assign the ID to that object
-            if same_object_detected is False:
-                self.center_points[self.id_count] = (cx, cy)
-                # objects_bbs_ids.append([x, y, w, h, self.id_count])
-                rect[4]=self.id_count
-                self.id_count += 1
-
-        # Clean the dictionary by center points to remove IDS not used anymore
-        new_center_points = {}
-        for rect in objects_rect:
-            _, _, _, _, object_id = rect
-            center = self.center_points[object_id]
-            new_center_points[object_id] = center
-
-        # Update dictionary with IDs not used removed
-        self.center_points = new_center_points.copy()
-        # print(self.center_points)
-        # return objects_bbs_ids
+        self.center_points_prev_frame = center_points_cur_frame.copy()
