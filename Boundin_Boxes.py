47fc513dfbe6f0a2e69792ba6f0b9e17974999f5
alexandra7578 <110526731+alexandra7578@users.noreply.github.com>
2024-03-06 10:46:18 +0200

    Add files via upload

--- Boundin_Boxes.py
+++ Boundin_Boxes.py
@@ -2,6 +2,7 @@ import pandas as pd
 from datetime import datetime, timedelta
 from tracker import Tracker
 import time
+from sort import *
 
 class BoundingBoxExtractor:
     def __init__(self, model, class_list):
@@ -10,22 +11,66 @@ class BoundingBoxExtractor:
         self.previous_time = datetime.now()
         self.static_vehicle_ids = set()
         self.dynamic_vehicle_ids = set()
-        self.tracker = Tracker()  # Presupunând că aveți o clasă Tracker pentru urmărire
+        # self.tracker = Tracker()  # Presupunând că aveți o clasă Tracker pentru urmărire
         self.tracking_objects = {}
         self.bbox_list = []  # Adăugăm aceste variabile la nivel de clasă
         self.availability_time_list = []
+        self.tracker = Sort()
 
+    # def get_bounding_boxes(self, img):
+    #     results = self.model.predict(img)
+    #     boxes = results[0].boxes.data
+    #     px = pd.DataFrame(boxes).astype("float")
+    #     self.bbox_list = []  # Resetăm listele la fiecare apel
+    #     self.availability_time_list = []
+    #
+    #     current_time = datetime.now()
+    #     elapsed_time = current_time - self.previous_time
+    #     self.previous_time = current_time
+    #     detections = []
+    #     for _, row in px.iterrows():
+    #         x1 = int(row[0])
+    #         y1 = int(row[1])
+    #         x2 = int(row[2])
+    #         y2 = int(row[3])
+    #         d = int(row[5])
+    #
+    #         c = self.class_list[d]
+    #         if 'car' in c and y1 > 75:
+    #             detections.append([x1, y1, x2, y2])
+    #             # self.bbox_list.append([x1, y1, x2, y2, 0])  # 0 în locul pentru ID-ul tracker-ului
+    #             # self.availability_time_list.append(elapsed_time.total_seconds())
+    #
+    #     # Actualizează tracker-ul cu noile bounding box-uri
+    #     self.tracker.update(self.bbox_list)
+    #
+    #     # Obține toate obiectele de urmărire curente
+    #     current_tracks = self.tracker.get_current_tracks()
+    #
+    #     # Actualizează informațiile despre obiectele de urmărire
+    #     for track_id, bbox in current_tracks.items():
+    #         if track_id not in self.tracking_objects:
+    #             self.tracking_objects[track_id] = {'last_appearance_time': time.time(), 'status': 'dynamic'}
+    #
+    #         # Calculează timpul scurs de la ultima apariție
+    #         elapsed_time = time.time() - self.tracking_objects[track_id]['last_appearance_time']
+    #
+    #         # Actualizează status-ul în funcție de pragul de timp (30 secunde în exemplul de mai jos)
+    #         if elapsed_time >= 30:
+    #             self.tracking_objects[track_id]['status'] = 'static'
+    #         else:
+    #             self.tracking_objects[track_id]['status'] = 'dynamic'
+    #
+    #         # Actualizează timpul ultimei apariții
+    #         self.tracking_objects[track_id]['last_appearance_time'] = time.time()
+    #     return detections
+    #     # return self.bbox_list, self.availability_time_list
     def get_bounding_boxes(self, img):
         results = self.model.predict(img)
         boxes = results[0].boxes.data
         px = pd.DataFrame(boxes).astype("float")
-        self.bbox_list = []  # Resetăm listele la fiecare apel
-        self.availability_time_list = []
 
-        current_time = datetime.now()
-        elapsed_time = current_time - self.previous_time
-        self.previous_time = current_time
-
+        detections = []
         for _, row in px.iterrows():
             x1 = int(row[0])
             y1 = int(row[1])
@@ -35,34 +80,18 @@ class BoundingBoxExtractor:
 
             c = self.class_list[d]
             if 'car' in c and y1 > 75:
-                self.bbox_list.append([x1, y1, x2, y2, 0])  # 0 în locul pentru ID-ul tracker-ului
-                self.availability_time_list.append(elapsed_time.total_seconds())
+                detections.append([x1, y1, x2, y2, 1]) #1 represent the score
 
-        # Actualizează tracker-ul cu noile bounding box-uri
-        self.tracker.update(self.bbox_list)
+        # Actualizăm tracker-ul SORT cu detecțiile
+        track_bbs_ids = self.update_tracker(detections)
 
-        # Obține toate obiectele de urmărire curente
-        current_tracks = self.tracker.get_current_tracks()
+        return track_bbs_ids
 
-        # Actualizează informațiile despre obiectele de urmărire
-        for track_id, bbox in current_tracks.items():
-            if track_id not in self.tracking_objects:
-                self.tracking_objects[track_id] = {'last_appearance_time': time.time(), 'status': 'dynamic'}
+    def update_tracker(self, detections):
+        return self.tracker.update(detections)
 
-            # Calculează timpul scurs de la ultima apariție
-            elapsed_time = time.time() - self.tracking_objects[track_id]['last_appearance_time']
-
-            # Actualizează status-ul în funcție de pragul de timp (30 secunde în exemplul de mai jos)
-            if elapsed_time >= 30:
-                self.tracking_objects[track_id]['status'] = 'static'
-            else:
-                self.tracking_objects[track_id]['status'] = 'dynamic'
-
-            # Actualizează timpul ultimei apariții
-            self.tracking_objects[track_id]['last_appearance_time'] = time.time()
-
-        return self.bbox_list, self.availability_time_list
-
+    def get_tracking_info(self):
+        return self.tracker.get_current_tracks()
     def update(self):
         current_time = datetime.now()
 
